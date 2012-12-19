ccc597491c0e148c745fdc93d7cee06aa148857d
richard <richard@DeveloperWorkstation.local>
2012-12-19 15:45:45 +1300

    make it work with binary plugins

--- src/com/bluetrainsoftware/bluegrails/idea/BluegrailsMavenImporter.java
+++ src/com/bluetrainsoftware/bluegrails/idea/BluegrailsMavenImporter.java
@@ -18,10 +18,7 @@ import org.jetbrains.idea.maven.importing.MavenRootModelAdapter;
 import org.jetbrains.idea.maven.model.MavenArtifact;
 import org.jetbrains.idea.maven.model.MavenId;
 import org.jetbrains.idea.maven.model.MavenPlugin;
-import org.jetbrains.idea.maven.project.MavenProject;
-import org.jetbrains.idea.maven.project.MavenProjectChanges;
-import org.jetbrains.idea.maven.project.MavenProjectsProcessorTask;
-import org.jetbrains.idea.maven.project.MavenProjectsTree;
+import org.jetbrains.idea.maven.project.*;
 import org.jetbrains.idea.maven.server.MavenEmbedderWrapper;
 import org.jetbrains.idea.maven.server.NativeMavenProjectHolder;
 import org.jetbrains.idea.maven.utils.MavenProcessCanceledException;
@@ -35,12 +32,22 @@ public class BluegrailsMavenImporter extends MavenImporter {
     super("com.bluetrainsoftware.bluegrails", "grails-maven-plugin");
   }
 
-  
-  
+
+  @Override
+  public void getSupportedDependencyTypes(Collection<String> result, SupportedRequestType type) {
+    result.add("jar");
+    result.add("grails-plugin2");
+  }
+
   public void resolve(Project project, MavenProject mavenProject, NativeMavenProjectHolder nativeMavenProject, MavenEmbedderWrapper embedder)
     throws MavenProcessCanceledException
   {
-    // there is nothing to do here, what is imported is correct
+//    for (MavenArtifact artifact : mavenProject.getDependencies()) {
+//      if ("grails-plugin2".equals(artifact.getType())) {
+//        mavenProject.addDependency(artifact);
+//      }
+//    }
+    // needs to also import grails-plugin2 projects
   }
 
   public void preProcess(Module module, MavenProject mavenProject, MavenProjectChanges changes, MavenModifiableModelsProvider modifiableModelsProvider)
